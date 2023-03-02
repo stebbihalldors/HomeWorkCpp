be5ec1d2bf26a103f1442a2e75c557434dc79081
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-03-02 13:22:29 +0100

    Adds PrintOutAlphabet Homework

---
+++ HelloWorld/String With All Letters/String With All Letters.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+
+void PrintOutAlphabet();
+
+int main()
+{
+	PrintOutAlphabet();
+   
+}
+
+void PrintOutAlphabet()
+{
+	int const alphabetCount = 26;
+	char text[alphabetCount];
+
+	for (size_t i = 0; i < alphabetCount; ++i)
+	{
+		text[i] = 65+i;
+	}
+
+	for (size_t i = 0; i < alphabetCount; ++i)
+	{
+		printf("%c ", text[i]);
+	}
+}
