a332a3a35720e1c267a7bb85dbbc951398575382
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-03-07 14:28:37 +0100

    Updating Age classwork with error handling

--- HelloWorld/Age Classwork/Age Classwork.cpp
+++ HelloWorld/Age Classwork/Age Classwork.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <cstdio>
+using namespace std;
 
 int main()
 {
     int age;
     int ans;
-    std::cout << "What's your age?" << std::endl;
-    std::cin >> age;
-    std::cout << "Next year, you'll be " << age+1 << " !" << std::endl;
-       
-   
+    //new c++ style with streaming operators
+    cout << "What's your age?" << endl;
+    while (!(cin >> age))
+    {
+        cin.clear();
+        cin.ignore(1000000, '\n');
+        cout << "That's not a valid input, try again." << endl;
+    }
+    cout << "Next year, you'll be " << age + 1 << " !" << std::endl;
+
+    //c style
+    AskAgain:
+    printf("What's your age?\n");
+    int result = scanf_s("%d", &ans);
+
+    { // clear unparsed characters from the buffer
+        char c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    if (result != 1) goto AskAgain;
+
+    printf("Next year, you'll be %d", ans + 1);
 }
