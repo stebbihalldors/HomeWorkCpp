1fc352aada929a73e48b055f7ab0f9ca4c6b4a90
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-03-14 11:09:53 +0100

    Update 24-Calculator.cpp

--- HelloWorld/24-Calculator/24-Calculator.cpp
+++ HelloWorld/24-Calculator/24-Calculator.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
+#include <functional>
 
 enum Operation {
-    Add = 5,
+    Add,
+    Subtract,
+    Multiply,
+    Divide
 };
 
 class Calculator{
 private:
-    int number;
+    int number1;
+    int number2;
+    std::function<int(int, int)> op;
 
 
 public:
     Calculator()
     {
-        number = 0;
+        number1 = 0;
+        number2 = 0;
     }
 
-    Calculator(int a)
+    /*Calculator(int a, int b)
     {
-        number = a;
-    }
+        number1 = a;
+        number2 = b;
+    }*/
 
     ~Calculator()
     {
@@ -26,30 +34,53 @@ public:
     }
 
     void SetNumber(int a) {
-        number = a;
+        if (number1 == 0)
+            number1 = a;
+        else
+            number2 = a;
     }
 
     int GetNumber() {
-        return number;
+        return op(number1,number2);
     }
 
     void SetOperation(Operation a)
     {
         switch (a) {
         case 0:
-            //save a plus :?
+            //std::function<int(int, int)> op = std::plus<int>();
+            op = std::plus<int>();
             break;
+        case 1:
+            op = std::minus<int>();
         default:
             break;
         }
+    }
+
+    void Clear()
+    {
+        number1 = 0;
+        number2 = 0;
     }
     
 };
 
 int main()
-{
+{/*
+    Calculator calc(2, 5);
+    printf("%d\n", calc.GetNumber());
+    */
     Calculator calculator;
     calculator.SetNumber(5);
-    Operation::Add;
-    calculator.GetNumber();
+    calculator.SetOperation(Operation::Add);
+    calculator.SetNumber(3);
+    printf("%d\n", calculator.GetNumber());
+
+    calculator.Clear();
+    calculator.SetNumber(5);
+    calculator.SetOperation(Operation::Subtract);
+    calculator.SetNumber(3);
+    printf("%d\n", calculator.GetNumber());
+
 }
